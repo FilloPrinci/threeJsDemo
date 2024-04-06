d8c1c245e351c9f792902b7e93bd4bd70863d3a4
FilloPrinci <fipri98@gmail.com>
2024-04-06 22:13:34 +0200

    feat: set up project, showing a rotating cube

---
+++ src/scene.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+
+// Youtube tutorial for next steps : https://www.youtube.com/watch?v=EJaoZTgDK2s
+
+export function createScene(){
+    // Initial scene setup
+    const gameWindow = document.getElementById('render-target');
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x777777);
+    
+    const camera = new THREE.PerspectiveCamera(75, gameWindow.offsetWidth / gameWindow.offsetHeight, 0.1, 1000);
+
+camera.position.z = 5;
+
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
+    gameWindow.appendChild(renderer.domElement);
+
+
+        //Set up geometry and material for cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial(
+        {
+            color: 0xff0000
+        }
+    );
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+
+    function draw(){
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+
+    function start(){
+        renderer.setAnimationLoop(draw);
+    }
+
+    function stop(){
+        renderer.setAnimationLoop(null);
+    }
+
+    return{
+        start,
+        stop
+    }
+}
